c1cb281f66ec243fee4d86f5a099fc97311c50d7
diaken <diakendiaken@gmail.com>
2022-12-01 06:54:07 +0800

    Change font family

--- src/pages/KelolaTransaksi.jsx
+++ src/pages/KelolaTransaksi.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import Sidebar from "../components/sidebar/Sidebar";
 const KelolaTransaksi = () => {
   return (
-    <div>KelolaTransaksi</div>
-  )
-}
+    <div className="d-flex">
+      <Sidebar list={3}></Sidebar>
+      <div className="mt-5 pt-4">Transaksi</div>
+    </div>
+  );
+};
 
-export default KelolaTransaksi
+export default KelolaTransaksi;
