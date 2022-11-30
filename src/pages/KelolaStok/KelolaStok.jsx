a378e632bff7e8f34cf33f3891fdd247bf05da3a
Dery <mderyd00@gmail.com>
2022-11-30 14:16:19 +0700

    Uodate sidebar

--- src/pages/KelolaStok/KelolaStok.jsx
+++ src/pages/KelolaStok/KelolaStok.jsx
@@ -9,6 +9,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import StokPaketData from "./StokPaketData";
 import { Button, Card } from "react-bootstrap";
 import { AiOutlinePlusSquare } from "react-icons/ai";
+import { TextField } from '@mui/material';
+import {BiSearch, BiSortDown} from 'react-icons/bi'
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -55,31 +57,35 @@ export default function KelolaStok() {
       <div className="d-flex">
         <Sidebar />
         <div className="mt-5 pt-5 ps-3 pe-3 w-100">
-          <Card className="d-flex p-3">
-            <p className="mb-4 mt-1">Kelola Stok</p>
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-              >
+        <Card className='d-flex'>
+          <div style={{ backgroundColor: 'whitesmoke' }}>
+          <p className='mb-4 mt-1 ps-3 pt-3'>Kelola Stok</p>
+            <div className="d-flex ps-3" >
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                 <Tab label="Pulsa" {...a11yProps(0)} />
                 <Tab label="Paket Data" {...a11yProps(1)} />
               </Tabs>
-            </Box>
-            <div className="d-flex flex-row mb-3 mt-3">
-              <Button>
-                {" "}
-                <AiOutlinePlusSquare
-                  style={{
-                    width: "20px",
-                    height: "25px",
-                    marginRight: "5px",
-                    paddingBottom: "3px",
-                  }}
-                />
-                Tambah Produk
-              </Button>
+            </div>
+          </div>
+            <div className='d-flex flex-row justify-content-between mb-3 mt-3 ps-3'>
+              <Button variant='success'> 
+              {""}
+              <AiOutlinePlusSquare style={{width: "20px",
+              height : "25px", marginRight: "5px", 
+              paddingBottom: "3px"}}/>
+              Tambah Produk</Button>
+              <div className='d-flex flex-row gap-2 pe-3'>
+              <div className="">
+              <TextField
+                id="search"
+                variant="outlined"
+                label={<p style={{fontSize:'13px', fontWeight:'540'}}><BiSearch style={{height:'20px', width:'20px'}}/>Cari</p>}
+                size='small'
+              /></div>
+              <div>
+                <BiSortDown style={{height: '40px', width: '30px'}}/>
+              </div>
+              </div>
             </div>
             <TabPanel value={value} index={0}>
               <StokPulsa />
