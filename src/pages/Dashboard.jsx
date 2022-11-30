9265f71e6e764c97732f00d4eac6b7b842e2c7ec
diaken <diakendiaken@gmail.com>
2022-11-30 13:12:30 +0800

    Add Design for ui sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Dashboard() {
   return (
--- src/pages/KelolaPengguna/KelolaPengguna.jsx
+++ src/pages/KelolaPengguna/KelolaPengguna.jsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Sidebar from '../../components/Sidebar';
-import ListUser from './ListUser';
-import ListAdmin from './ListAdmin';
-import { Button, Card } from 'react-bootstrap';
-import {AiOutlinePlusSquare} from 'react-icons/ai';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Sidebar from "../../components/sidebar/Sidebar";
+import ListUser from "./ListUser";
+import ListAdmin from "./ListAdmin";
+import { Button, Card } from "react-bootstrap";
+import { AiOutlinePlusSquare } from "react-icons/ai";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -39,7 +39,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -52,33 +52,44 @@ export default function KelolaPengguna() {
 
   return (
     <>
-    <div className="d-flex">
-      <Sidebar/>
-      <div className="mt-5 pt-5 ps-3 pe-3 w-100">
-      <Card className='d-flex p-3'>
-      <p>Kelola Pengguna</p>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="User" {...a11yProps(0)} />
-            <Tab label="Admin" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        <div className='d-flex flex-row mb-3 mt-3'>
-          <Button> <AiOutlinePlusSquare style={{width: "20px",
-           height : "25px", marginRight: "5px", 
-           paddingBottom: "3px"}}/>Tambah User</Button>
+      <div className="d-flex">
+        <Sidebar />
+        <div className="mt-5 pt-5 ps-3 pe-3 w-100">
+          <Card className="d-flex p-3">
+            <p>Kelola Pengguna</p>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+              >
+                <Tab label="User" {...a11yProps(0)} />
+                <Tab label="Admin" {...a11yProps(1)} />
+              </Tabs>
+            </Box>
+            <div className="d-flex flex-row mb-3 mt-3">
+              <Button>
+                {" "}
+                <AiOutlinePlusSquare
+                  style={{
+                    width: "20px",
+                    height: "25px",
+                    marginRight: "5px",
+                    paddingBottom: "3px",
+                  }}
+                />
+                Tambah User
+              </Button>
+            </div>
+            <TabPanel value={value} index={0}>
+              <ListUser />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <ListAdmin />
+            </TabPanel>
+          </Card>
         </div>
-        <TabPanel value={value} index={0}>
-          <ListUser/>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <ListAdmin/>
-        </TabPanel>
-      </Card>
       </div>
-    </div>      
-   
     </>
-      
   );
 }
