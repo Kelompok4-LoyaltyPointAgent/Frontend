a531508c9dc778c22f05d137cdf272e41bdd80b7
Dery <mderyd00@gmail.com>
2022-12-01 15:14:12 +0700

    Update page detail produk pulsa, mr.d

--- src/pages/KelolaStok/DetailEditProdukPulsa.jsx
+++ src/pages/KelolaStok/DetailEditProdukPulsa.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
-import { Card, CardImg, Button} from 'react-bootstrap'
+import { Card, Button, Row, Col} from 'react-bootstrap'
 import Sidebar from '../../components/sidebar/Sidebar'
 import '../../assets/styles/DetailEditProduk.css'
 import EditProdukPulsa from './EditProdukPulsa';
@@ -14,7 +14,6 @@ const DetailEditProdukPulsa = () => {
     setIsOpen(!isOpen);
   };
 
-
   const handleGoBack = ()=>{
     navigate(-1);
   }
@@ -25,8 +24,33 @@ const DetailEditProdukPulsa = () => {
       <div className='mt-5 pt-5 ps-3 pe-3 w-100'>
       {isOpen && <EditProdukPulsa handleClose={togglePopUp} />}
         <Card border='dark'>
-          <CardImg style={{padding: '10px', height: '250px', marginBottom:'20px'}} src={require('../../assets/images/Banner Pulsa.png')}/>
-          <div className='d-flex justify-content-between list-detail'>
+          <Card.Img style={{padding:'10px', height:'250px'}} src={require('../../assets/images/ProdukPulsa/Vector 2.png')}/>
+          <Card.ImgOverlay>
+              <Row>
+                <Col sm={2} lg={2} className='d-flex justify-content-end'>
+                <i><img style={{height:'120px'}}
+                alt='iconproduk' src={require('../../assets/images/ProdukPulsa/Frame 33773.png')}/></i>
+                </Col>
+                <Col sm={8} lg={8} className='d-flex flex-column justify-content-center align-items-center'>
+                <p className='mt-4' style={{color:'white'}}><h4>Detail Produk</h4></p>
+                <p className='text-harga'><h2>Harga</h2></p>
+                <p className='text-provider'>Provider</p>
+                </Col>
+                <Col sm={2} lg={2} className='d-flex justify-content-end pe-4'>
+                <div><img style={{marginTop:'13px',height:'110px',width:'125px'
+                , zIndex:'2', top:'0px',position:'relative'}} 
+                alt='iconpoin' src={require('../../assets/images/ProdukPulsa/Rectangle 15.png')}/>
+                <div className='text-center' style={{padding:'3px', zIndex:'9', position:'absolute', top:'30px', color: '#013B75'}}>
+                  <p style={{margin:'0px', marginTop:'2px', marginLeft:'30px'}}><h5>Reward</h5></p>
+                  <p style={{margin:'0px', marginTop:'2px', marginLeft:'30px'}}><h4>777</h4></p>
+                  </div>
+                </div>
+                </Col>
+              </Row>
+          </Card.ImgOverlay>
+          <Card.Body>
+          <Card.Text>
+          <div className='mt-2 d-flex justify-content-between list-detail'>
             <p className='detail-produk-text'>Masa Aktif</p>
             <p className='detail-produk-text'>60 Hari</p>
           </div>
@@ -45,11 +69,14 @@ const DetailEditProdukPulsa = () => {
           <div style={{marginLeft:'100px'}}>
             <p>Deskripsi</p>
             <p>Dapatkan pulsa 10.000 dengan harga Rp 12.000 Harga sudah termasuk PPN 10%</p>
+          </div><br/>
+          <div style={{position:'relative'}}className='mb-2 d-flex justify-content-center gap-5'>
+            <Button style={{backgroundColor:'#006BA0', borderColor:'#006BA0'}} onClick={togglePopUp} >Ubah</Button>
+            <Button style={{backgroundColor:'#D15F02', borderColor:'#D15F02'}} onClick={handleGoBack}>Kembali</Button>
           </div>
-          <div className='mt-3 mb-4 d-flex justify-content-center gap-5'>
-            <Button onClick={togglePopUp} >Ubah</Button>
-            <Button variant='danger' onClick={handleGoBack}>Kembali</Button>
-          </div>
+          </Card.Text>
+          </Card.Body>
+          
         </Card>
       </div>
     </div>
