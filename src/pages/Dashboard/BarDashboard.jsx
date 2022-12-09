a10e5fcc4df99690b208157ca253e27500634ae5
Dery <mderyd00@gmail.com>
2022-12-09 20:31:11 +0700

    Fix ui pagination, mr.d

--- src/pages/Dashboard/BarDashboard.jsx
+++ src/pages/Dashboard/BarDashboard.jsx
@@ -1,39 +1,64 @@
 import React from 'react'
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as 
+    ChartJS, 
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend, } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
 import { Card } from 'react-bootstrap'
 
 ChartJS.register(
     CategoryScale,
-    LinearScale,
-    BarElement
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 )
 
 const BarDashboard = () => {
     var data = {
         labels: ['01', '02', '03', '04', '05', '06', '07','08','09','10','11','12'],
-        
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
         datasets: [{
-            label: '# of Votes',
-            
+            label: 'Pembelian',
             data: [12, 19, 15, 5, 8, 6, 10, 17, 21, 25, 20, 19],
             borderWidth: 1,
             backgroundColor: [
                 '#FBB505'
             ],
+            borderRadius: 5,
             
-            
-            
-        }]
+        }],
     }
 
     var options = {
         maintainAspectRatio: false,
         scales: {
+            x: {
+                grid: {
+                    display: false,
+                }
+            },
             y: {
+                grid: {
+                    display: false,
+                },
                 beginAtZero: true
             }
         }
+    ,
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+        },
+        label:{
+            display: false
+        }
+    
     }
 
     return (
