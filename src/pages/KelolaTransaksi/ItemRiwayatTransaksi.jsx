79860062bd218f642299b1e9d6da90fd72dcc716
Dery <mderyd00@gmail.com>
2022-12-13 21:43:17 +0700

    Update page Transaksi, mr.d

--- src/pages/KelolaTransaksi/ItemRiwayatTransaksi.jsx
+++ src/pages/KelolaTransaksi/ItemRiwayatTransaksi.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import "../../assets/styles/Button.css";
+import {FiPhone} from 'react-icons/fi'
 
 const ItemRiwayatTransaksi = ({ data, index }) => {
     return (
       <tr
         style={
-          index % 2 == 0
+          index % 2 === 0
             ? { backgroundColor: "#F5F6F7" }
             : { backgroundColor: "#FEF0CD" }
         }
       >
-        <td className="col-1 text-start">{data.provider}</td>
-        <td className="col-1">{data.name}</td>
-        <td className="col-1">{data.stock}</td>
-        <td className="col-1">{data.price_points}</td>
-        <td className="col-1">{data.price}</td>
-        <td className="col-1">{data.price}</td>
-        <td className="col-1">{data.price}</td>
+        <td className="col-2 text-center">{data.created_date}</td>
+        <td className="col-1">{data.transaction_detail.email}<br/>
+        <FiPhone style={{fontSize:'16px', marginRight:'5px', marginBottom:'4px'}}/>{data.transaction_detail.number}</td>
+        <td className="col-1">{data.type}</td>
+        <td className="col-1">{data.method}</td>
+        <td className="col-1">{data.amount}</td>
+        <td className="col-1">{data.status}</td>
         <td className="col-1">
-          <i className="bi bi-file-earmark-text file-button"></i>
           <i className="bi bi-trash3 ms-3 delete-button"></i>
         </td>
       </tr>
