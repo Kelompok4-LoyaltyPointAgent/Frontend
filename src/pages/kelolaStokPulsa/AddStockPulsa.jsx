25e1e10b7b8695d66d27d194af9bdf099bbc7b79
diaken <diakendiaken@gmail.com>
2022-12-19 21:56:41 +0800

    Change Value data on pulsa and paket

--- src/pages/kelolaStokPulsa/AddStockPulsa.jsx
+++ src/pages/kelolaStokPulsa/AddStockPulsa.jsx
@@ -139,7 +139,7 @@ const AddStockPulsa = (props) => {
                 <option required value="Telkomsel">
                   Telkomsel
                 </option>
-                <option required value="Xl">
+                <option required value="XL">
                   Xl
                 </option>
                 <option required value="Indosat">
